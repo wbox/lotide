836c959d4e05ed06d6ff2fec1a0434dec0a1011c
Sam <sribas@gmail.com>
2021-03-02 04:13:32 +0000

    Fix: Implementing assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\uD83D\uDE00 Assertion Passed: [actual] === [expected]");
+    console.log("\uD83D\uDE00 Assertion Passed: " + actual + " === " + expected);
   } else {
-    console.log("\uD83D\uDE1E Assertion Failed: [actual] !== [expected]");
+    console.log("\uD83D\uDE1E Assertion Failed: " + actual + " !== " + expected);
   }
 };
 
