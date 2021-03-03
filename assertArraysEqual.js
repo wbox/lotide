8bb4954f854071d9acff8b993871fb8762b3aab6
Sam <sribas@gmail.com>
2021-03-03 04:05:06 +0000

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertArrayEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA,arrayB)) {
+    console.log(`\uD83D\uDE00 Assertion Passed: ${arrayA} == ${arrayB}`);
+  } else {
+    console.log(`\uD83D\uDE1E Assertion Failed: ${arrayA} != ${arrayB}`);
+  }
+};
+
+const eqArrays = function (arrayA, arrayB) {
+  // Check if the parameters are arrays and have the same size
+  if ( (!Array.isArray(arrayA) || !Array.isArray(arrayB)) || (arrayA.length !== arrayB.length) ) {
+    return false;
+  }
+  // Check if the elements are equal
+  for ( let i = 0; i < arrayA.length; i++) {
+    if (!(arrayA[i] === arrayB[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+
+
+assertArrayEqual([1,2,3],[1,2,3]);
+assertArrayEqual([1,2,3],[1,"2",3]);
+assertArrayEqual([1,2,3]);
+
