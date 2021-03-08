afc188e31286319607cd25c4d6b2315a5f55fa86
Sam <sribas@gmail.com>
2021-03-08 22:49:54 +0000

    Week2/Day1 - Exporting all functions

---
+++ index.js
@@ -0,0 +1,102 @@
+// index.js
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const index = require('./index');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const package = require('./package');
+const package-lock = require('./package-lock');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const index = require('./index');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const package = require('./package');
+const package-lock = require('./package-lock');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const index = require('./index');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const package = require('./package');
+const package-lock = require('./package-lock');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const index = require('./index');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const package = require('./package');
+const package-lock = require('./package-lock');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+	assertArraysEqual: 	assertArraysEqual,
+	assertEqual: 	      assertEqual,
+	assertObjectsEqual: assertObjectsEqual,
+	countLetters: 	    countLetters,
+	countOnly: 	        countOnly,
+	eqArrays: 	        eqArrays,
+	eqObjects: 	        eqObjects,
+	findKeyByValue: 	  findKeyByValue,
+	findKey: 	          findKey,
+	flatten: 	          flatten,
+	head: 	            head,
+	letterPositions: 	  letterPositions,
+	map: 	              map,
+	middle: 	          middle,
+	tail: 	            tail,
+	takeUntil: 	        takeUntil,
+	without: 	          without
+};
