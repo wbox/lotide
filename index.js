a4b3bee7e9144a72d24d94acc983d933497be840
Sam <sribas@gmail.com>
2021-03-08 23:43:08 +0000

    Week2/Day1 - Implement Object Shorthand

--- index.js
+++ index.js
@@ -17,24 +17,22 @@ const tail = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
-
-
 module.exports = {
-	assertArraysEqual: 	assertArraysEqual,
-	assertEqual: 	      assertEqual,
-	assertObjectsEqual: assertObjectsEqual,
-	countLetters: 	    countLetters,
-	countOnly: 	        countOnly,
-	eqArrays: 	        eqArrays,
-	eqObjects: 	        eqObjects,
-	findKeyByValue: 	  findKeyByValue,
-	findKey: 	          findKey,
-	flatten: 	          flatten,
-	head: 	            head,
-	letterPositions: 	  letterPositions,
-	map: 	              map,
-	middle: 	          middle,
-	tail: 	            tail,
-	takeUntil: 	        takeUntil,
-	without: 	          without
+	assertArraysEqual,
+	assertEqual,
+	assertObjectsEqual,
+	countLetters,
+	countOnly,
+	eqArrays,
+	eqObjects,
+	findKeyByValue,
+	findKey,
+	flatten,
+	head,
+	letterPositions,
+	map,
+	middle,
+	tail,
+	takeUntil,
+	without
 };
